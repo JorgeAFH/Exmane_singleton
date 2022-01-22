eba35253d290b91918fe31c0b89f63763dceddb5
JorgeAFH <e1316596319@live.uleam.edu.ec>
2022-01-21 21:31:34 -0500

    se agrega el proyecto

---
+++ Examen/Examen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    public class Examen
+    {
+        private static Examen instance = null;
+        public string nota_recordatorio = "";
+
+        protected Examen()
+        {
+            nota_recordatorio = "hoy toca lavar los platos";
+        }
+
+        public static Examen Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Examen();
+
+                return instance;
+            }
+        }
+
+    }
+}
---
+++ Examen/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen 
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Examen.Instance.nota_recordatorio);
+            Examen.Instance.nota_recordatorio = "hoy toca barrer";
+            Console.WriteLine(Examen.Instance.nota_recordatorio);
+            Examen.Instance.nota_recordatorio = "hoy toca limpiar los carros";
+            Console.WriteLine(Examen.Instance.nota_recordatorio);
+        }
+    }
+}
